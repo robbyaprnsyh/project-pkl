f83a3e480d36c68e241256b217bec6a31b06aa91
robbyaprnsyh <robbyapriansyah979@gmail.com>
2024-06-24 12:56:00 +0700

    login & register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,159 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en" data-bs-theme="blue-theme">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png">
+    <!-- loader-->
+    <link href="{{ asset('backend/assets/css/pace.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!--plugins-->
+    <link href="{{ asset('backend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/plugins/metismenu/metisMenu.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/plugins/metismenu/mm-vertical.css') }}">
+    <!--bootstrap css-->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined" rel="stylesheet">
+    <!--main css-->
+    <link href="{{ asset('backend/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/dark-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/blue-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/sass/responsive.css') }}" rel="stylesheet">
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+</head>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+<body class="bg-register">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+
+    <!--authentication-->
+
+    <div class="container-fluid my-5">
+        <div class="row">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-5 mx-auto">
+                <div class="card rounded-4 mb-0 border-top border-4 border-primary border-gradient-1">
+                    <div class="card-body p-5">
+                        <img src="{{ asset('backend/assets/images/logo1.png') }}" class="mb-4" width="145" alt="">
+                        <h4 class="fw-bold">Get Started Now</h4>
+                        <p class="mb-0">Enter your credentials to create your account</p>
+
+                        <div class="form-body my-4">
+                            <form class="row g-3" method="POST">
+                                @csrf
+                                <div class="col-12">
+                                    <label for="inputUsername" class="form-label">Username</label>
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputEmailAddress" class="form-label">Email</label>
+
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email">
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputChoosePassword" class="form-label">Password</label>
+
+                                    {{-- <div class="input-group">
+                                        <input id="password" type="password"
+                                            class="form-control @error('password') is-invalid @enderror" name="password"
+                                            required autocomplete="new-password"><a href="javascript:;"
+                                            class="input-group-text bg-transparent"><i
+                                                class="bi bi-eye-slash-fill"></i></a>
+                                    </div> --}}
+
+                                    <div class="input-group" id="show_hide_password">
+                                        <input type="password" class="form-control" id="password" name="password"
+                                            required placeholder="Enter Password" autocomplete="new-password">
+                                        <a href="javascript:;" class="input-group-text bg-transparent"><i
+                                                class="bi bi-eye-slash-fill"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Confirm
+                                        Password</label>
+
+                                    {{-- <div class="input-group">
+                                        <input id="password-confirm" type="password" class="form-control"
+                                            name="password_confirmation" required autocomplete="new-password"><a
+                                            href="javascript:;" class="input-group-text bg-transparent"><i
+                                                class="bi bi-eye-slash-fill"></i></a>
+                                    </div> --}}
+
+                                    <div class="input-group" id="show_hide_password_confirm">
+                                        <input type="password" class="form-control " id="password-confirm"
+                                            name="password_confirmation" placeholder="Enter Password" required
+                                            autocomplete="new-password">
+                                        <a href="javascript:;" class="input-group-text bg-transparent"><i
+                                                class="bi bi-eye-slash-fill"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="d-grid">
+                                        <button type="submit" class="btn btn-grd-danger">Register</button>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="text-start">
+                                        <p class="mb-0">Already have an account? <a
+                                                href="{{ route('login') }}">Sign
+                                                in here</a></p>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div><!--end row-->
     </div>
-</div>
-@endsection
+
+    <!--authentication-->
+
+
+
+
+    <!--plugins-->
+    <script src="{{ asset('backend/assets/js/jquery.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bi-eye-slash-fill");
+                    $('#show_hide_password i').removeClass("bi-eye-fill");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bi-eye-slash-fill");
+                    $('#show_hide_password i').addClass("bi-eye-fill");
+                }
+            });
+        });
+        $(document).ready(function() {
+            $("#show_hide_password_confirm a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password_confirm input').attr("type") == "text") {
+                    $('#show_hide_password_confirm input').attr('type', 'password');
+                    $('#show_hide_password_confirm i').addClass("bi-eye-slash-fill");
+                    $('#show_hide_password_confirm i').removeClass("bi-eye-fill");
+                } else if ($('#show_hide_password_confirm input').attr("type") == "password") {
+                    $('#show_hide_password_confirm input').attr('type', 'text');
+                    $('#show_hide_password_confirm i').removeClass("bi-eye-slash-fill");
+                    $('#show_hide_password_confirm i').addClass("bi-eye-fill");
+                }
+            });
+        });
+    </script>
+
+</body>
+
+</html>
